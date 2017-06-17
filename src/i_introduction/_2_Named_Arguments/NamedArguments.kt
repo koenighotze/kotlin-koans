2f4429adfbd92a5331f81ea9287e82b9499e6219
David Schmitz <david.schmitz.privat@gmail.com>
2017-06-17 15:59:23 +0200

    WIP

--- src/i_introduction/_2_Named_Arguments/NamedArguments.kt
+++ src/i_introduction/_2_Named_Arguments/NamedArguments.kt
@@ -1,9 +1,5 @@
 package i_introduction._2_Named_Arguments
 
-import i_introduction._1_Java_To_Kotlin_Converter.task1
-import util.TODO
-import util.doc2
-
 // default values for arguments:
 fun bar(i: Int, s: String = "", b: Boolean = true) {}
 
@@ -12,16 +8,6 @@ fun usage() {
     bar(1, b = false)
 }
 
-fun todoTask2(): Nothing = TODO(
-    """
-        Task 2.
-        Implement the same logic as in 'task1' again through the library method 'joinToString()'.
-        Specify only two of the 'joinToString' arguments.
-    """,
-    documentation = doc2(),
-    references = { collection: Collection<Int> -> task1(collection); collection.joinToString() })
-
 fun task2(collection: Collection<Int>): String {
-    todoTask2()
-    return collection.joinToString()
+    return collection.joinToString(prefix = "{", postfix = "}")
 }
